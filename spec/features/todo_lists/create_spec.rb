2ca4d2fec9a4fd92fdad592e59a1c3c9fe9ad2a2
Etai Klein <etaiklein@gmail.com>
2014-06-12 18:12:14 -0400

    updated spec

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -51,6 +51,45 @@ describe "Creating todo lists" do
     visit "/todo_lists"
     expect(page).to_not have_content("run an rspec test")
 
+  end
+
+  it "displays an error when the todo list has a description is blank" do
+    expect (TodoList.count).should eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New todo_list")
+
+    fill_in "Title", with: "Valid Title"
+    fill_in "Description", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect (TodoList.count).should eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("hi")
+
+
+  end
+
+  it "displays an error when the todo list has a description less than 5 char" do
+    expect (TodoList.count).should eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New todo_list")
+
+    fill_in "Title", with: "Valid Title"
+    fill_in "Description", with: "hi"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect (TodoList.count).should eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("hi")
+
 
   end
 end
